f7ec62d6ced8adc6e53d7279b0846871aa74a496
にやおぱすー <3284869028@qq.com>
2021-05-04 07:52:33 +0800

    new subbmit 202105040752 readme.c new line

--- readme.c
+++ readme.c
@@ -2,7 +2,11 @@
 
 int main(int argc, char* argv) {
 	char string_out[] = "This is a readme ... C";
+
 	printf("%s", string_out);
+	printf("Git is a distributed version control system.");
+	printf("Git is free software.");
+
 
 	return 0;
 } 
